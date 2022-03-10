4f9c1aef8588ddef2388d61779d58b5542bb3912
Xander <xandercernek@gmail.com>
2022-03-10 12:01:01 -0600

    Typescript Hello World

--- test.ts
+++ test.ts
@@ -1,6 +1,26 @@
-class Card {
+class Minion {
+    mana: number;
+    attack: number;
+    health: number;
+    constructor(_mana: number, _attack: number, _health: number) {
+        this.mana = _mana;
+        this.attack = _attack;
+        this.health = _health;
+    }
 }
 
-enum Phase {
-    
+class Player {
+    name: string;
+    health: number;
+    constructor(_name: string, _health: number) {
+        this.name = _name;
+        this.health = _health;
+    }
 }
+
+function battle() {
+    let message: string = 'Hello, World!';
+    console.log(message);
+}
+
+battle();
